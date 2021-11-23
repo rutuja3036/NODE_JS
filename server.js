ead73e24d2cbefb2d15f9f7acb894296a52b15df
rutuja <rutujaingale0230@example.com>
2021-11-23 20:35:12 +0530

    response object

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url,req.method);
+  //set header context type
+  res.setHeader('Content-Type','text/html');
+
+
+  res.write('hello,ninjas</P>');
+  res.write('<p>hello again, ninjas</p>');
+  res.end();
 });
   server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000');
