81f8a4a9f1b4954cb31efaf82dc248787edb1da0
rutuja <rutujaingale0230@example.com>
2021-11-23 21:09:26 +0530

    returing html pages

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url,req.method);
+
   //set header context type
   res.setHeader('Content-Type','text/html');
 
-
-  res.write('hello,ninjas</P>');
-  res.write('<p>hello again, ninjas</p>');
-  res.end();
-});
+  // send html file
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    else{
+      res.write(data);
+    res.end(data);
+    }
+   })
+  });
   server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000');
   });
+  
